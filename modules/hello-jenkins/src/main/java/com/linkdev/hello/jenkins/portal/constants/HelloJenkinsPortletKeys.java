e25ccf8580f64401d55971714b22806059ff2332
Mahmoud Mamdouh Abdullah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2024-10-19 20:43:19 +0300

    hello-jenkins

---
+++ modules/hello-jenkins/src/main/java/com/linkdev/hello/jenkins/portal/constants/HelloJenkinsPortletKeys.java
@@ -0,0 +1,11 @@
+package com.linkdev.hello.jenkins.portal.constants;
+
+/**
+ * @author Mahmoud.Abdullah
+ */
+public class HelloJenkinsPortletKeys {
+
+	public static final String HELLOJENKINS =
+		"com_linkdev_hello_jenkins_portal_HelloJenkinsPortlet";
+
+}
---
+++ modules/hello-jenkins/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<h1>Testing Pipeling</h1>
+</p>
