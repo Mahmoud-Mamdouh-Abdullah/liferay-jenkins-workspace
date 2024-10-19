b200fe8f70b4928e6ed9e085acb844ca40a840fb
Mahmoud Mamdouh Abdullah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2024-10-19 15:31:04 +0300

    add-test-module

---
+++ modules/test-pipeline/src/main/java/com/linkdev/test/constants/TestPipelinePortletKeys.java
@@ -0,0 +1,11 @@
+package com.linkdev.test.constants;
+
+/**
+ * @author Mahmoud.Abdullah
+ */
+public class TestPipelinePortletKeys {
+
+	public static final String TESTPIPELINE =
+		"com_linkdev_test_TestPipelinePortlet";
+
+}
