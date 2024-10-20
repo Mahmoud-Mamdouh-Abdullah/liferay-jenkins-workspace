fb3ec24e96df031cc8b17fb7106b41c376caaed0
Mahmoud Mamdouh Abdullah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2024-10-20 11:35:21 +0300

    test-module

---
+++ modules/test-module/src/main/java/com/linkdev/test/module/constants/TestModulePortletKeys.java
@@ -0,0 +1,11 @@
+package com.linkdev.test.module.constants;
+
+/**
+ * @author Mahmoud.Abdullah
+ */
+public class TestModulePortletKeys {
+
+	public static final String TESTMODULE =
+		"com_linkdev_test_module_TestModulePortlet";
+
+}
