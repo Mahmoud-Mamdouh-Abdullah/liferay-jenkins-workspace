6dd165e58202f4906dc4720690873b89302ecb6a
Mahmoud Mamdouh Abdullah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2024-10-21 17:44:41 +0300

    Update LastUpdateDatetimePagePortlet.java

--- modules/last-update-datetime/src/main/java/com/linkdev/mohap/lastupdatedatetime/web/portlet/LastUpdateDatetimePagePortlet.java
+++ modules/last-update-datetime/src/main/java/com/linkdev/mohap/lastupdatedatetime/web/portlet/LastUpdateDatetimePagePortlet.java
@@ -28,7 +28,7 @@ import java.util.Locale;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.instanceable=false",
 		"javax.portlet.display-name=Last Update Date and Time For The Page",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
